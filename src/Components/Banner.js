e589ba611e901d6e5180eb85513fe6516c3f3030
macei <lucacscoutinho@gmail.com>
2023-06-02 17:28:41 -0300

    sexta 02/06

---
+++ src/Components/Banner.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Banner = function (){
+    <div class="bg-secondary">ssdt4red</div>
+}
+
+export default Banner;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = function (){
+    return (
+        <div class="nav p-4 bg-dark">
+            <a class="nav-item nav-link text-light" href="#">Link1</a>
+            <a class="nav-item nav-link text-light" href="#">Link1</a>
+            <a class="nav-item nav-link text-light" href="#">Link1</a>
+            <a class="nav-item nav-link text-light" href="#">Link1</a>
+            <a class="nav-item nav-link text-light" href="#">Link1</a>
+        </div>
+    );
+}
+
+export default Navbar;
